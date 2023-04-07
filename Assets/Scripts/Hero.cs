b1a75db8aeff90542cfca1a6342838c07531a499
Roman Mesler <pay2mesler@gmail.com>
2023-04-07 21:12:38 +0500

    add gitignore and finish taks 1 P.S try to fix isGrounded

---
+++ Assets/Scripts/Hero.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.WSA;
+
+public class Hero : MonoBehaviour
+{
+    [SerializeField] private float speed = 3f;//скорость движения
+    [SerializeField] private int lives = 5;//количество жизней;
+    [SerializeField] private float jumpForce = 15f; //сила прыжка;
+
+
+    private Rigidbody2D rb;
+    private SpriteRenderer sprite;
+    public bool isGrounded = false;
+    public float rayDistance = 0.6f;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    private void FixedUpdate()
+    {
+        CheckGround();
+    }
+
+    private void Update()
+    {
+        
+        if (Input.GetButton("Horizontal"))
+        {
+            Run();
+        }
+
+        if (isGrounded || Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(new Vector2(0, jumpForce),ForceMode2D.Impulse);
+        }
+    }
+    
+    private void Run()
+    {
+        Vector3 dir = transform.right * Input.GetAxis("Horizontal");
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + dir, speed * Time.deltaTime);
+        sprite.flipX = dir.x < 0.0f;
+    }
+
+    private void CheckGround()
+    {
+        Collider2D[] collider = Physics2D.OverlapCircleAll(transform.position, 1.5f);
+        isGrounded = collider.Length > 1;
+    }
+}
