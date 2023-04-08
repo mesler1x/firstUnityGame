a0b5e50d660d13ea3ba979ebaece0f7303917940
Roman Mesler <pay2mesler@gmail.com>
2023-04-08 12:59:37 +0500

    add animation and camera | finish task 2

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -15,16 +15,27 @@ public class Hero : MonoBehaviour
     private SpriteRenderer sprite;
     public bool isGrounded = false;
     public float rayDistance = 0.6f;
+    private Animator anim;
 
+    private States State
+    {
+        get{ return (States)anim.GetInteger("state"); }
+        set { anim.SetInteger("state", (int)value); }
+    }
+        
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         sprite = GetComponentInChildren<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
     
     private void Update()
     {
-        
+        if (isGrounded)
+        {
+            State = States.idle;
+        }
         if (Input.GetButton("Horizontal"))
         {
             Run();
@@ -38,6 +49,7 @@ public class Hero : MonoBehaviour
     
     private void Run()
     {
+        if (isGrounded) State = States.run;
         Vector3 dir = transform.right * Input.GetAxis("Horizontal");
         transform.position = Vector3.MoveTowards(transform.position, transform.position + dir, speed * Time.deltaTime);
         sprite.flipX = dir.x < 0.0f;
@@ -51,5 +63,13 @@ public class Hero : MonoBehaviour
     private void OnCollisionExit2D(Collision2D collision)
     {
         isGrounded = false;
+        State = States.jump;
     }
+}
+
+public enum States
+{
+    idle,
+    run,
+    jump
 }
